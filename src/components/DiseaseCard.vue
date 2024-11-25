f71409b1cb56dbd743647896407a3138d47affe5
PieroNiccolai <pieroniccolai996@gmail.com>
2024-11-25 22:20:14 +0100

    created two new components

---
+++ src/components/DiseaseCard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-col justify-center">
+        Ciao
+    </div>
+</template>
---
+++ src/components/FilterContainer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="bg-violet-950 text-white">
+        Ciao aaa
+    </div>
+</template>
