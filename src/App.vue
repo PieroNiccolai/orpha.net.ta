bf371c5e9017b1b33b6ab9b897f79bce0106e73f
Piero Niccolai <piero.niccolai@aidia.it>
2024-11-27 19:56:20 +0100

    kind of sstarted css pagination...

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import DiseaseCard from './components/DiseaseCard.vue';
-import FilterContainer from './components/FilterContainer.vue';
+
+let name=''
 </script>
 
 <template>
-  <div class="flex flex-col justify-items-center bg-violet-950 text-white w-full h-full">
+  <div class="flex flex-col justify-items-center items-center bg-violet-950 text-white w-full h-full">
+    <div class="flex flex-row justify-center items-center space-x-5 mt-5">
+    <div class="flex flex-col text-black">
+      <input v-model="name" type="text" name="Disease name" id="disease_name">
+    </div>
+    <div v-if="name.length>0" class="flex flex-col text-black">
+      <input type="text" name="Disease name" id="disease_name">
+    
+  </div>
+
+</div>
+    
     <DiseaseCard/>
-    <FilterContainer/>
   </div>
   
 </template>
